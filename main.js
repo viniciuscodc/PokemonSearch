fce53609777957084b50650c8536767f998e9673
viniciuscodc <viniciuscodc@gmail.com>
2021-04-04 19:10:38 -0300

    Styled texts

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const pokemonName = "lucario";
-var apiURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
+let submit = document.querySelector(".search");
 
-async function getData() {
-  const response = await fetch(apiURL);
-  const data = await response.json();
+submit.addEventListener("click", function () {
+  var pokemonName = document.getElementById("pokemonName").value;
 
-  var getImage = data["sprites"]["other"]["official-artwork"]["front_default"];
-  document.getElementById("pokemon").src = getImage;
-}
-getData();
+  //const pokemonName = "lucario";
+  var apiURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
+
+  async function getData() {
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      var getImage = data["sprites"]["other"]["official-artwork"]["front_default"];
+      document.getElementById("pokemon").src = getImage;
+    } catch {
+      console.log("error");
+    }
+  }
+  getData();
+});
