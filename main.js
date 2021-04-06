5a5453c47ba29c858639f9916c3f16b83a1cfec1
viniciuscodc <viniciuscodc@gmail.com>
2021-04-06 04:10:54 -0300

    Name and number display

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-let submit = document.querySelector(".search");
+var submit = document.querySelector("#submit");
 
 submit.addEventListener("click", function () {
-  var pokemonName = document.getElementById("pokemonName").value;
-
-  //const pokemonName = "lucario";
-  var apiURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
+  var userInput = document.getElementById("userInput").value.toLowerCase();
+  const apiURL = "https://pokeapi.co/api/v2/pokemon/" + userInput;
 
   async function getData() {
     try {
       const response = await fetch(apiURL);
-      const data = await response.json();
-      var getImage = data["sprites"]["other"]["official-artwork"]["front_default"];
-      document.getElementById("pokemon").src = getImage;
+      var data = await response.json();
     } catch {
       console.log("error");
     }
+    var getImage =
+      data["sprites"]["other"]["official-artwork"]["front_default"];
+    document.getElementById("pokemonSprite").src = getImage;
+
+    function displayName() {
+      let getName = data.name;
+      let nameText = document.querySelector(".name");
+      nameText.innerHTML = getName;
+      nameText.style.setProperty("text-transform", "capitalize");
+    }
+    function displayNumber() {
+      let getNumber = data.id;
+      let numberText = document.querySelector(".number");
+      numberText.innerHTML = "#" + getNumber;
+    }
+
+    displayNumber();
+    displayName();
+    document.querySelector(".container").style.opacity = 1;
+    document.querySelector(".container2").style.opacity = 1;
   }
+
   getData();
 });
